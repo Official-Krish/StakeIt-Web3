538aa5cdd6313f0713fd0575408c6e6e7089eb1f
Official-Krish <krishanand0103@gmail.com>
2025-06-08 18:52:25 +0530

    [feat]: added custom token minting logic

--- programs/staking-contract/src/lib.rs
+++ programs/staking-contract/src/lib.rs
@@ -1,6 +1,16 @@
-use anchor_lang::prelude::*;
 use anchor_lang::system_program;
+use anchor_spl::{associated_token::AssociatedToken, token::{MintTo, TokenAccount}};
 
+use {
+    anchor_lang::prelude::*,
+    anchor_spl::{
+        metadata::{
+            create_metadata_accounts_v3, mpl_token_metadata::types::DataV2,
+            CreateMetadataAccountsV3,Metadata
+        },
+        token::{Mint, Token, mint_to},
+    },
+};
 
 declare_id!("3zwUW9CAKvJU4MYaKmuKCUn7CmZYsdAX5FMFt3dqbhnG");
 
@@ -10,7 +20,10 @@ const SECONDS_PER_DAY: u64 = 86_400;
 
 #[program]
 pub mod staking_contract {
+
     use super::*;
+    use anchor_spl::token::{self, Burn};
+    
 
     pub fn create_pda_account(ctx: Context<CreatePdaAccount>) -> Result<()> {
         let pda_account = &mut ctx.accounts.pda_account;
@@ -21,6 +34,29 @@ pub mod staking_contract {
         pda_account.bump = ctx.bumps.pda_account;
 
         msg!("PDA Account created successfully!");
+        Ok(())
+    }
+
+    pub fn create_token_mint(ctx: Context<CreateToken>, _token_decimals: u8, token_name: String, token_symbol: String, token_uri: String) -> Result<()>{
+
+        create_metadata_accounts_v3(
+            CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                CreateMetadataAccountsV3 {
+                    metadata: ctx.accounts.metadata_account.to_account_info(),
+                    mint: ctx.accounts.mint_account.to_account_info(),
+                    mint_authority: ctx.accounts.payer.to_account_info(),
+                    payer: ctx.accounts.payer.to_account_info(),
+                    update_authority: ctx.accounts.payer.to_account_info(),
+                    system_program: ctx.accounts.system_program.to_account_info(),
+                    rent: ctx.accounts.rent.to_account_info(),
+                },
+            ),
+            DataV2 { name: (token_name), symbol: (token_symbol), uri: (token_uri), seller_fee_basis_points: 0, creators: None, collection: None, uses: None },
+            false, // is_mutable
+            true, // is_collection
+            None, // collection_details
+        )?;
         Ok(())
     }
 
@@ -36,11 +72,24 @@ pub mod staking_contract {
             ctx.accounts.system_program.to_account_info(),
             system_program::Transfer {
                 from: ctx.accounts.payer.to_account_info(),
-                to: pda_account.to_account_info(),
+                to: ctx.accounts.vault.to_account_info(),
             },
         );
 
         system_program::transfer(cpi_context, amount)?;
+
+        mint_to(
+            CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                MintTo {
+                    mint: ctx.accounts.mint_account.to_account_info(),
+                    to: ctx.accounts.associated_token_account.to_account_info(),
+                    authority: ctx.accounts.payer.to_account_info(),
+                },
+            ),
+            amount * 10u64.pow(ctx.accounts.mint_account.decimals as u32)
+        )?;
+
         pda_account.staked_amount = pda_account.staked_amount.checked_add(amount)
             .ok_or(StakeError::Overflow)?;
 
@@ -50,6 +99,8 @@ pub mod staking_contract {
 
     pub fn unstake(ctx: Context<Unstake>,amount: u64) -> Result<()> {
         require!(amount > 0, StakeError::InvalidAmount);
+        require!(ctx.accounts.pda_account.staked_amount >= amount, StakeError::InvalidAmount);
+
         let pda_account = &mut ctx.accounts.pda_account;
 
         require!(pda_account.staked_amount >= amount, StakeError::InsufficientStake);
@@ -58,26 +109,36 @@ pub mod staking_contract {
 
         update_points(pda_account, current_timestamp)?;
 
-        // let user_key = ctx.accounts.user.key(); 
-        // let seeds = &[
-        //     b"pda_account",
-        //     user_key.as_ref(),
-        //     &[pda_account.bump],
-        // ];
-        // let signer = &[&seeds[..]];
-        // let cpi_context = CpiContext::new_with_signer(
-        //     ctx.accounts.system_program.to_account_info(),
-        //     system_program::Transfer {
-        //         from: pda_account.to_account_info(),
-        //         to: ctx.accounts.user.to_account_info(),
-        //     },
-        //     signer,
-        // );
+        let signer_key = ctx.accounts.user.key(); 
+        let vault_seeds = &[
+            b"vault",
+            signer_key.as_ref(),
+            &[ctx.bumps.vault],
+        ];
+        let vault_signer = &[&vault_seeds[..]];
 
-        **pda_account.to_account_info().try_borrow_mut_lamports()? -= amount;
-        **ctx.accounts.user.to_account_info().try_borrow_mut_lamports()? += amount;
+        let cpi_context = CpiContext::new_with_signer(
+            ctx.accounts.system_program.to_account_info(),
+            system_program::Transfer {
+                from: ctx.accounts.vault.to_account_info(),
+                to: ctx.accounts.user.to_account_info(),
+            },
+            vault_signer,
+        );
+        system_program::transfer(cpi_context, amount * LAMPORTS_PER_SOL)?;
 
-        pda_account.staked_amount = pda_account.staked_amount.checked_sub(amount)
+        let burn_cpi_context = CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            Burn {
+                mint: ctx.accounts.mint_account.to_account_info(),
+                from: ctx.accounts.associated_token_account.to_account_info(),
+                authority: ctx.accounts.user.to_account_info(),
+            },
+        );
+
+        token::burn(burn_cpi_context, amount * 10u64.pow(ctx.accounts.mint_account.decimals as u32))?;
+
+        ctx.accounts.pda_account.staked_amount = pda_account.staked_amount.checked_sub(amount)
             .ok_or(StakeError::Underflow)?;
         msg!("Unstake successful! Amount: {}", amount);
         Ok(())
@@ -152,18 +213,28 @@ fn calculate_points(staked_amount: u64, time_elapsed: i64) -> Result<u64> {
 }
 
 #[derive(Accounts)]
-pub struct CreatePdaAccount <'info> {
+pub struct CreatePdaAccount<'info> {
     #[account(mut)]
     pub payer: Signer<'info>,
+    
     #[account(
         init,
         payer = payer,
-        space = 8 + 32 + 8 + 8 + 8 + 1,
+        space = 8 + 32 + 8 + 8 + 8 + 1, 
         seeds = [b"pda_account", payer.key().as_ref()],
         bump,
     )]
-
     pub pda_account: Account<'info, StakeAccount>,
+
+    #[account(
+        init,
+        payer = payer,
+        seeds = [b"vault", payer.key().as_ref()],
+        bump,
+        space = 8,
+    )]
+    pub vault: SystemAccount<'info>,
+    
     pub system_program: Program<'info, System>,
 }
 
@@ -178,6 +249,25 @@ pub struct Stake<'info> {
         constraint = pda_account.owner == payer.key() @ StakeError::Unauthorized,
     )]
     pub pda_account: Account<'info, StakeAccount>,
+
+    #[account(mut)]
+    pub mint_account: Account<'info, Mint>,
+    #[account(
+        init_if_needed,
+        payer = payer,
+        associated_token::mint = mint_account,
+        associated_token::authority = payer
+    )]
+    pub associated_token_account: Account<'info, TokenAccount>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+
+    #[account(
+        mut,
+        seeds = [b"vault", payer.key().as_ref()],
+        bump
+    )]
+    pub vault: SystemAccount<'info>,
     pub system_program: Program<'info, System>,
 }
 
@@ -185,6 +275,7 @@ pub struct Stake<'info> {
 pub struct Unstake<'info> {
     #[account(mut)]
     pub user: Signer<'info>,
+
     #[account(
         mut,
         seeds = [b"pda_account", user.key().as_ref()],
@@ -192,6 +283,21 @@ pub struct Unstake<'info> {
         constraint = pda_account.owner == user.key() @ StakeError::Unauthorized,
     )]
     pub pda_account: Account<'info, StakeAccount>,
+
+    pub mint_account: Account<'info, Mint>,
+    #[account(
+        mut,
+        associated_token::mint = mint_account,
+        associated_token::authority = user
+    )]
+    pub associated_token_account: Account<'info, TokenAccount>,
+    pub token_program: Program<'info, Token>,
+    #[account(
+        mut,
+        seeds = [b"vault", user.key().as_ref()],
+        bump
+    )]
+    pub vault: SystemAccount<'info>,
     pub system_program: Program<'info, System>,
 }
 
@@ -219,9 +325,35 @@ pub struct GetPoints<'info> {
     pub user: Signer<'info>,
 }
 
+#[derive(Accounts)]
+#[instruction( _token_decimals: u8, token_name: String, token_symbol: String, token_uri: String )]
+pub struct CreateToken<'info>{
+    #[account(mut)]
+    pub payer:Signer<'info>,
+
+    #[account(
+        mut,
+        seeds=[b"metadata", token_metadata.key().as_ref(), mint_account.key().as_ref()],bump, seeds::program=token_metadata.key()
+    )]
+
+    pub metadata_account:UncheckedAccount<'info>,
+
+    #[account(
+        init,
+        payer = payer,
+        mint::decimals = _token_decimals,
+        mint::authority = payer.key()
+    )]
+    pub mint_account:Account<'info, Mint>,
+    pub token_metadata:Program<'info, Metadata>,
+    pub token_program:Program<'info, Token>,
+    pub system_program:Program<'info, System>,
+    pub rent:Sysvar<'info, Rent>
+} 
+
 #[account]
 pub struct StakeAccount {
-   pub owner: Pubkey,
+    pub owner: Pubkey,
     pub staked_amount: u64,
     pub total_points: u64,
     pub last_updated_timestamp: i64,
